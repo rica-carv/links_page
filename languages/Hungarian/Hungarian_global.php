eb16b185719004fa4e525c54d5b25187dd698919
Gábor <yesszus@gmail.com>
2016-09-21 10:44:21 +0200

    Update Hungarian_global.php
    Move to pharses Hungarian_front.php

--- languages/Hungarian/Hungarian_global.php
+++ languages/Hungarian/Hungarian_global.php
@@ -259,3 +259,12 @@ define("LAN_LINKS_51", "összes link megtekintése");
 define('LAN_LINKS_SCH_1','Beküldött válasz a linkhez');
 
 define("LCLAN_ADMIN_14", "Linkek");
+
+define("LCLAN_PAGETITLE_2", "Kategóriák");
+define("LCLAN_PAGETITLE_3", "Linkek");
+define("LCLAN_PAGETITLE_4", "kategória");
+define("LCLAN_PAGETITLE_5", "Legtöbb Értékelés");
+define("LCLAN_PAGETITLE_6", "Legtöbb Hivatkozás");
+define("LCLAN_PAGETITLE_7", "Link Kezelő");
+define("LCLAN_PAGETITLE_8", "Hozzászólások");
+define("LCLAN_PAGETITLE_9", "Link beküldése");
