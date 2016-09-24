d788ecc0c494c6c706d3af25e079c9efe1325efc
Jimmi08 <janamicek@gmail.com>
2016-09-24 14:08:54 +0200

    Update Slovak Lang Pack

--- languages/Slovak/Slovak_global.php
+++ languages/Slovak/Slovak_global.php
@@ -214,5 +214,11 @@ define("LAN_LINKS_51", "Zobraziť všetky odkazy");
 define("LAN_LINKS_SCH_1", "Posted in reply to link");
 define("LCLAN_ADMIN_14", "Links");
 
-
-?>
+define("LCLAN_PAGETITLE_2", "Všetky kategórie");
+define("LCLAN_PAGETITLE_3", "Všetky odkazy");
+define("LCLAN_PAGETITLE_4", "kategória");
+define("LCLAN_PAGETITLE_5", "Najviac hodnotené");
+define("LCLAN_PAGETITLE_6", "Najviac kliknutí");
+define("LCLAN_PAGETITLE_7", "Manažér vlastných odkazov");
+define("LCLAN_PAGETITLE_8", "Komentáre k odkazom");
+define("LCLAN_PAGETITLE_9", "Odoslať odkaz");
